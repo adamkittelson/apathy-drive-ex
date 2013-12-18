d4daae7ab4a84336ceb52d70e6e3e626313167fe
Adam Kittelson <adam.kittelson@apathydrive.com>
2013-12-17 21:07:34 -0600

    make tests pass

--- test/response_test.exs
+++ test/response_test.exs
@@ -2,7 +2,7 @@ defmodule WeberHttpResponseTest do
   use ExUnit.Case
 
   test "SimpleResponse test" do
-    {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/weber', [], <<>>, [])
+    {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:3000/weber', [], <<>>, [])
     assert(status == 404)
   end
 
