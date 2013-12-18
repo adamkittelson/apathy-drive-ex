a64aec19ca9c0c0d617d81b84d2ddd6b027547af
Adam Kittelson <adam.kittelson@apathydrive.com>
2013-12-17 20:59:47 -0600

    add bootstrap, basic websockets

--- lib/config.ex
+++ lib/config.ex
@@ -3,7 +3,7 @@ defmodule Config do
   def config do
     [webserver:
       [http_host: "localhost",
-       http_port: 8080,
+       http_port: 3000,
        acceptors: 100,
        ssl: false,
        cacertfile_path: "",
@@ -11,8 +11,8 @@ defmodule Config do
        keyfile_path: ""
       ],
     ws:
-      [ws_port: 8080,
-       ws_mod: :Handler
+      [ws_port: 3000,
+       ws_mod: :ApathyDrive.Main
       ],
     use_internationalization: false,
     localization:
--- lib/controllers/main.ex
+++ lib/controllers/main.ex
@@ -7,4 +7,16 @@ defmodule ApathyDrive.Main do
     {:render, [], []}
   end
 
+  def websocket_init(pid, conn) do
+    IO.puts "Connected! pid: #{inspect pid}"
+  end
+
+  def websocket_message(pid, message, conn) do
+    IO.puts "Message! Pid: #{inspect pid}, Message: #{inspect message}"
+  end
+
+  def websocket_terminate(pid, conn) do
+    IO.puts "Terminated! Pid: #{inspect pid}"
+  end
+
 end
