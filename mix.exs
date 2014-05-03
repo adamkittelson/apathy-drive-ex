367d948db7f2d8fa6808fe7d46708eb60de892ce
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-05-03 15:52:26 -0500

    update exjson

--- mix.exs
+++ mix.exs
@@ -20,6 +20,7 @@ defmodule ApathyDrive.Mixfile do
     [
       { :weber,    github: "elixir-web/weber", ref: "3132e10754b7ce8942b13eb08b34e55009592c6d" },
       { :ecto,     github: "elixir-lang/ecto", ref: "303131c50b5881fb7836ddbe2e88e0241c5e83c6" },
+      { :exjson,   github: "adamkittelson/exjson", ref: "0745ea9eb3af3105ad9235907fd51620fa927709", override: true},
       { :postgrex, "~> 0.4.2"},
       { :bcrypt,   github: "Feuerlabs/erlang-bcrypt"}
     ]
