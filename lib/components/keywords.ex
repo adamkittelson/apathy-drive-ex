5a0e2d2aa5d2cf706377a4dad797c99d26d52a09
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-05-03 23:11:29 -0500

    code to match a string against partial or full names / keywords of entities

---
+++ lib/components/keywords.ex
@@ -0,0 +1,33 @@
+defmodule Components.Keywords do
+  use GenEvent.Behaviour
+
+  ### Public API
+  def value(entity) do
+    :gen_event.call(entity, Components.Keywords, :value)
+  end
+
+  def value(entity, new_value) do
+    ApathyDrive.Entity.notify(entity, {:set_keywords, new_value})
+  end
+
+  def serialize(entity) do
+    {"Keywords", value(entity)}
+  end
+
+  ### GenEvent API
+  def init(exit_ids) do
+    {:ok, exit_ids}
+  end
+
+  def handle_call(:value, exit_ids) do
+    {:ok, exit_ids, exit_ids}
+  end
+
+  def handle_event({:set_keywords, new_value}, _value) do
+    {:ok, new_value }
+  end
+
+  def handle_event(_, current_value) do
+    {:ok, current_value}
+  end
+end
---
+++ lib/systems/match.ex
@@ -0,0 +1,106 @@
+defmodule Systems.Match do
+  def all(pids, :match_name, string) do
+    Enum.filter(pids, &(match_name(string, &1)))
+  end
+
+  def all(pids, :match_keyword, string) do
+    case all(pids, :match_name, string) do
+      []    -> Enum.filter(pids, &(match_keyword(string, &1)))
+      value -> value
+    end
+  end
+
+  def all(pids, :name_starts_with, string) do
+    case all(pids, :match_keyword, string) do
+      []    -> Enum.filter(pids, &(name_starts_with(string, &1)))
+      value -> value
+    end
+  end
+
+  def all(pids, :keyword_starts_with, string) do
+    case all(pids, :name_starts_with, string) do
+      []    -> Enum.filter(pids, &(keyword_starts_with(string, &1)))
+      value -> value
+    end
+  end
+
+  def all(pids, :name_contains, string) do
+    case all(pids, :keyword_starts_with, string) do
+      []    -> Enum.filter(pids, &(name_contains(string, &1)))
+      value -> value
+    end
+  end
+
+  def first(pids, :match_name, string) do
+    Enum.find(pids, &(match_name(string, &1)))
+  end
+
+  def first(pids, :match_keyword, string) do
+    case first(pids, :match_name, string) do
+      nil   -> Enum.find(pids, &(match_keyword(string, &1)))
+      value -> value
+    end
+  end
+
+  def first(pids, :name_starts_with, string) do
+    case first(pids, :match_keyword, string) do
+      nil   -> Enum.find(pids, &(name_starts_with(string, &1)))
+      value -> value
+    end
+  end
+
+  def first(pids, :keyword_starts_with, string) do
+    case first(pids, :name_starts_with, string) do
+      nil   -> Enum.find(pids, &(keyword_starts_with(string, &1)))
+      value -> value
+    end
+  end
+
+  def first(pids, :name_contains, string) do
+    case first(pids, :keyword_starts_with, string) do
+      nil   -> Enum.find(pids, &(name_contains(string, &1)))
+      value -> value
+    end
+  end
+
+  def match_name(string, pid) do
+    String.downcase(string) == pid
+                               |> Components.Name.value
+                               |> String.downcase
+  end
+
+  def match_keyword(string, pid) do
+    if pid |> ApathyDrive.Entity.list_components |> Enum.member?(Components.Keywords) do
+      pid |> Components.Keywords.value
+          |> Enum.any?(fn (keyword) ->
+               String.downcase(string) == String.downcase(keyword)
+             end)
+    else
+      false
+    end
+  end
+
+  def name_starts_with(string, pid) do
+    pid |> Components.Name.value
+        |> String.downcase
+        |> String.starts_with?(string |> String.downcase)
+  end
+
+  def keyword_starts_with(string, pid) do
+    if pid |> ApathyDrive.Entity.list_components |> Enum.member?(Components.Keywords) do
+      pid |> Components.Keywords.value
+          |> Enum.any?(fn (keyword) ->
+               String.downcase(keyword) |> String.starts_with?(string |> String.downcase)
+             end)
+    else
+      false
+    end
+  end
+
+  def name_contains(string, pid) do
+    pid |> Components.Name.value
+        |> String.downcase
+        |> String.contains?(string |> String.downcase)
+  end
+
+end
