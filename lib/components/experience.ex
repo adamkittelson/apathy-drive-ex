1cd99d753c7ed5cc0572091ad98cceebd0c96251
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-07 14:36:14 -0500

    'score' command

---
+++ lib/components/experience.ex
@@ -0,0 +1,35 @@
+defmodule Components.Experience do
+  use Systems.Reload
+  use GenEvent.Behaviour
+
+  ### Public API
+  def value(entity) do
+    :gen_event.call(entity, Components.Experience, :value)
+  end
+
+  def value(entity, new_value) do
+    Entity.notify(entity, {:set_experience, new_value})
+  end
+
+  def serialize(entity) do
+    %{"Experience" => value(entity)}
+  end
+
+  ### GenEvent API
+  def init(value) do
+    {:ok, value}
+  end
+
+  def handle_call(:value, value) do
+    {:ok, value, value}
+  end
+
+  def handle_event({:set_experience, new_value}, _value) do
+    {:ok, new_value }
+  end
+
+  def handle_event(_, current_value) do
+    {:ok, current_value}
+  end
+
+end
