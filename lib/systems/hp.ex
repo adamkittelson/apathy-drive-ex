a91f185cf61bb50a04e71731bcfabf44c4b8e75f
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-07 19:44:40 -0500

    change max_hp formula

--- lib/systems/hp.ex
+++ lib/systems/hp.ex
@@ -1,8 +1,9 @@
 defmodule Systems.HP do
   use Systems.Reload
   def max_hp(entity) do
-    health_stat   = Components.Stats.value(entity)["health"]
-    level = Components.Level.value(entity)
-    round(((health_stat / 2.0)) + (((health_stat - 50) * level) / 16.0))
+    health = Systems.Stat.modified(entity, "health")
+    level  = Components.Level.value(entity)
+
+    4 * (level + 9) + Enum.max([10, level - 25]) * health
   end
 end
