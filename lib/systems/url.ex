f6c78f10ed434def984966c1165918320424ee02
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-12 22:19:08 -0500

    function to get random alphanumeric strings

---
+++ lib/systems/url.ex
@@ -0,0 +1,18 @@
+defmodule Systems.URL do
+  use Systems.Reload
+
+  def random(length \\ 5) do
+    (1..length)
+    |> Enum.reduce([], fn(_, codepoints) ->
+         [random_alphanumeric | codepoints]
+       end)
+    |> Enum.join
+  end
+
+  defp random_alphanumeric do
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    |> String.codepoints
+    |> Enum.shuffle
+    |> List.first
+  end
+end
