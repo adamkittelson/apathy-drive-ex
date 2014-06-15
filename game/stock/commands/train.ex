8eea417a84fa088a721601fa02b033b55a078653
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-14 20:56:49 -0500

    update skills/train for spirits

--- game/stock/commands/train.ex
+++ game/stock/commands/train.ex
@@ -5,10 +5,14 @@ defmodule Commands.Train do
 
   def execute(entity, arguments) do
     current_room = Components.CurrentRoom.get_current_room(entity)
-    if Entity.has_component?(current_room, Components.Trainer) do
-      Systems.Trainer.train(entity, current_room, arguments)
-    else
-      Components.Player.send_message(entity, ["scroll", "<p><span class='red'>You cannot TRAIN if you are not at a trainer!</span></p>"])
+
+    cond do
+      !Entity.has_component?(current_room, Components.Trainer) ->
+        Components.Player.send_message(entity, ["scroll", "<p><span class='red'>You cannot TRAIN if you are not at a trainer!</span></p>"])
+      Components.Spirit.value(entity) ->
+        Components.Player.send_message(entity, ["scroll", "<p>You need a body to do that.</p>"])
+      true ->
+        Systems.Trainer.train(entity, current_room, arguments)
     end
   end
 end
