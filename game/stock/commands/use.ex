e93903400464290d4e71d77731a0722f7bee5bcf
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-15 21:38:03 -0500

    'use' command

---
+++ game/stock/commands/use.ex
@@ -0,0 +1,23 @@
+defmodule Commands.Use do
+  use Systems.Command
+
+  def keywords, do: ["use", "cast"]
+
+  def execute(entity, arguments) do
+    [ability_name | target_name] = arguments
+                                   |> Enum.join(" ")
+                                   |> String.split(" at ")
+    case Systems.Match.all(Abilities.all, :name_contains, ability_name) do
+      [match] ->
+        Components.Module.value(match).execute(entity, target_name)
+      [] ->
+        Components.Player.send_message(entity, ["scroll", "<p>You don't know how to peform \"#{ability_name}\".</p>"])
+      matches ->
+        match_names = matches |> Enum.map &(Components.Name.value(&1))
+        Components.Player.send_message(entity, ["scroll", "<p><span class='red'>Please be more specific. You could have meant any of these:</span></p>"])
+        Enum.each match_names, fn(match_name) ->
+          Components.Player.send_message(entity, ["scroll", "<p>-- #{match_name}</p>"])
+        end
+    end
+  end
+end
