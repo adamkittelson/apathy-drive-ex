b15971897688d0a1a0a20a516bbe0ca05ccb27b5
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-26 20:09:25 -0500

    fix looking at monsters

--- lib/systems/monster.ex
+++ lib/systems/monster.ex
@@ -4,7 +4,7 @@ defmodule Systems.Monster do
   def spawn_monster(monster) do
     {:ok, entity} = Entity.init
     Entity.add_component(entity, Components.Name,        Components.Name.get_name(monster))
-    Entity.add_component(entity, Components.Description, Components.Description.get_description(monster))
+    Entity.add_component(entity, Components.Description, Components.Module.value(monster).properties[:description])
     Entity.add_component(entity, Components.Types, ["monster"])
     Entity.add_to_type_collection(entity)
     entity
