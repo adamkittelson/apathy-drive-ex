534a26eabb377e6a610d0e16fa301cb40758ccd9
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-06-30 22:06:58 -0500

    update to elixir 0.14.2

--- mix.exs
+++ mix.exs
@@ -18,12 +18,16 @@ defmodule ApathyDrive.Mixfile do
 
   defp deps(:prod) do
     [
-      { :weber,    github: "elixir-web/weber", ref: "bd79f3a3cf3c7be4586b9fe2e0251432202d5432" },
-      { :ecto,     "~> 0.2.0" },
-      { :jazz,     github: "meh/jazz", ref: "7af3b74e58eb1a3fc6b9874a2077efa420f6dfcc" },
-      { :postgrex, "~> 0.5.1" },
+      { :weber,    github: "elixir-web/weber", ref: "3399cab61481de794ca6af53eb4bdf45d94b5e3c" },
+      { :weberContrib, github: "elixir-web/weber-contrib", ref: "2d64328195408a0d690c64f4d4b5055581cffe23", override: true },
+      { :exjson,   github: "adamkittelson/exjson", ref: "5c51b256976d7e71e8638a7895423f58b5dd4b25", override: true },
+      { :ecto,     "~> 0.2.2" },
+      { :plug,     "~> 0.5.1" },
+      { :jazz,     "~> 0.1.2" },
+      { :postgrex, "~> 0.5.2" },
+      { :decimal,  "~> 0.2.2" },
       { :bcrypt,   github: "Feuerlabs/erlang-bcrypt"},
-      { :inflex,   "~> 0.2.1" }
+      { :inflex,   "~> 0.2.4" }
     ]
   end
 
