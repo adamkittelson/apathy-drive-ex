c24599bfb66e7f1aefbfba8565525433344a16ed
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-07-12 21:46:13 -0500

    update elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ApathyDrive.Mixfile do
   def project do
     [ app: :apathy_drive,
       version: "0.0.1",
-      elixir: "~> 0.14.2",
+      elixir: "~> 0.14.3",
       deps: deps ]
   end
 
