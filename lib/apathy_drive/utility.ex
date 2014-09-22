e75cc5e8f94df55156932aa4c841c6acbefbc946
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-09-22 13:38:05 -0500

    kill some puts's

--- lib/apathy_drive/utility.ex
+++ lib/apathy_drive/utility.ex
@@ -6,9 +6,6 @@ defmodule Utility do
     if Entity.has_component?(entity, Components.Socket) do
       socket = Components.Socket.value(entity)
       if socket do
-        IO.puts "socket: #{inspect socket}"
-        IO.puts "event: #{inspect event}"
-        IO.puts "message: #{inspect message}"
         Phoenix.Channel.reply socket, event, %{:html => message}
       end
     end
