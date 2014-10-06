354682603544e31cf0a1ed5985c685a25970f812
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-10-06 14:54:29 -0500

    update stop for possession

--- lib/apathy_drive/data/stock/commands/stop.ex
+++ lib/apathy_drive/data/stock/commands/stop.ex
@@ -3,14 +3,14 @@ defmodule Commands.Stop do
 
   def keywords, do: ["stop"]
 
-  def execute(entity, arguments) do
-    if Components.Spirit.value(entity) == true do
-      send_message(entity, "scroll", "<p>You need a body to do that.</p>")
-    else
-      if Enum.join(arguments, " ") == "hunting" do
-        send_message(entity, "scroll", "<p>You stop hunting.</p>")
-        Components.Hunting.value(entity, [])
-      end
+  def execute(spirit, nil, arguments) do
+    send_message(spirit, "scroll", "<p>You need a body to do that.</p>")
+  end
+
+  def execute(spirit, monster, arguments) do
+    if Enum.join(arguments, " ") == "hunting" do
+      send_message(spirit, "scroll", "<p>You stop hunting.</p>")
+      Components.Hunting.value(monster, [])
     end
   end
 end
