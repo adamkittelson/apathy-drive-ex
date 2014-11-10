4f407164becbb5a8fb98f10d332e50ca1f27b2ba
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-11-09 22:01:30 -0600

    change monster name color by alignment

---
+++ lib/apathy_drive/components/alignment.ex
@@ -0,0 +1,51 @@
+defmodule Components.Alignment do
+  use Systems.Reload
+  use GenEvent
+  import Utility
+
+  ### Public API
+  def value(monster) do
+    GenEvent.call(monster, Components.Alignment, :value)
+  end
+
+  def good?(monster) do
+    value(monster) < -50
+  end
+
+  def evil?(monster) do
+    value(monster) > 50
+  end
+
+  def neutral?(monster) do
+    !good?(monster) and !evil?(monster)
+  end
+
+  def add_evil_points(monster, amount_to_add) do
+    GenEvent.notify(monster, {:add_evil_points, amount_to_add})
+  end
+
+  def serialize(entity) do
+    %{"Alignment" => value(entity)}
+  end
+
+  ### GenEvent API
+  def init(value) do
+    {:ok, value}
+  end
+
+  def handle_call(:value, value) do
+    {:ok, value, value}
+  end
+
+  def handle_event({:add_evil_points, amount}, value) do
+    value = (value + amount)
+            |> min(300)
+            |> max(-200)
+
+    {:ok, value}
+  end
+
+  def handle_event(_, value) do
+    {:ok, value}
+  end
+end
