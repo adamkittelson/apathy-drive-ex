d7eed3f77245cac60b12f2df5b5905792b72f828
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-11-27 15:55:31 -0600

    implement class/race exits and remove level exits

--- lib/apathy_drive/systems/exits/level.ex
+++
@@ -1,3 +0,0 @@
-defmodule Systems.Exits.Level do
-  use Systems.Exit
-end
--- lib/apathy_drive/systems/exits/race.ex
+++ lib/apathy_drive/systems/exits/race.ex
@@ -1,20 +1,20 @@
-defmodule Systems.Exits.Class do
+defmodule Systems.Exits.Race do
   use Systems.Exit
 
   def move(spirit, nil, current_room, room_exit),  do: super(spirit, nil, current_room, room_exit)
   def move(nil, monster, current_room, room_exit), do: super(nil, monster, current_room, room_exit)
 
   def move(spirit, monster, current_room, room_exit) do
-    class = String.downcase(room_exit["class"])
+    race = String.downcase(room_exit["race"])
     name = Components.Name.value(monster) |> String.downcase
 
-    if String.contains?(name, class) do
+    if String.contains?(name, race) do
       super(spirit, monster, current_room, room_exit)
     else
-      send_message(monster, "scroll", "<p>Only #{Inflex.pluralize(room_exit["class"])} may go #{room_exit["direction"]} here.</p>")
+      send_message(monster, "scroll", "<p>Only #{Inflex.pluralize(room_exit["race"])} may go #{room_exit["direction"]} here.</p>")
       Systems.Monster.observers(current_room, monster)
       |> Enum.each(fn(observer) ->
-        send_message(observer, "scroll", "<p>#{capitalize_first(Components.Name.value(monster))} tries to go #{room_exit["direction"]} but only #{Inflex.pluralize(room_exit["class"])} may go that way.</span></p>")
+        send_message(observer, "scroll", "<p>#{capitalize_first(Components.Name.value(monster))} tries to go #{room_exit["direction"]} but only #{Inflex.pluralize(room_exit["race"])} may go that way.</span></p>")
       end)
     end
   end
