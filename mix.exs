cfc4b27e09062e09d1ddb23a51b4b80a180c0da7
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-11-30 15:27:31 -0600

    update phoenix

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ApathyDrive.Mixfile do
   defp deps do
     [
       {:cowboy,      "~> 1.0.0"},
-      {:phoenix,     "~> 0.6.0"},
+      {:phoenix,     "~> 0.6.1"},
       {:ecto,        "~> 0.2.4"},
       {:postgrex,    "~> 0.6.0"},
       {:timex,       "~> 0.12.7"},
