f163ba0bf843db707fd5231837df7ea5c05fa6f0
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-02-08 13:19:34 -0600

    abilities command skeleton

--- lib/apathy_drive/commands/abilities.ex
+++ lib/apathy_drive/commands/abilities.ex
@@ -8,34 +8,35 @@ defmodule Commands.Abilities do
     |> Spirit.send_scroll("<p>You must be possessing a monster to have abilities.</p>")
   end
 
-  def execute(spirit, monster, _arguments) do
-    send_message(spirit, "scroll", "<p><span class='white'>You have the following abilities:</span></p>")
-    send_message(spirit, "scroll", "<p><span class='dark-magenta'>Mana   Command  Ability Name</span></p>")
-    display_abilities(monster)
+  def execute(%Monster{} = monster, _arguments) do
+    monster
+    |> Monster.send_scroll("<p><span class='white'>You have the following abilities:</span></p>")
+    |> Monster.send_scroll("<p><span class='dark-magenta'>Mana   Command  Ability Name</span></p>")
+    |> display_abilities
   end
 
-  def display_abilities(entity) do
-    entity
-    |> Components.Abilities.value
-    |> Enum.map(fn(ability) ->
-         Components.Module.value(ability)
-       end)
-    |> Enum.filter(fn(module) ->
-         module.properties(entity)
-         |> Map.has_key?(:command)
-       end)
-    |> Enum.each(fn(module) ->
-         properties = module.properties(entity)
-         mana_cost = properties[:mana_cost]
-                     |> to_string
-                     |> String.ljust(6)
-
-         command = properties[:command]
-                   |> to_string
-                   |> String.ljust(8)
-
-         send_message(entity, "scroll", "<p><span class='dark-cyan'>#{mana_cost} #{command} #{module.name}</span></p>")
-       end)
+  def display_abilities(%Monster{} = monster) do
+    monster
+    # |> Components.Abilities.value
+    # |> Enum.map(fn(ability) ->
+    #      Components.Module.value(ability)
+    #    end)
+    # |> Enum.filter(fn(module) ->
+    #      module.properties(entity)
+    #      |> Map.has_key?(:command)
+    #    end)
+    # |> Enum.each(fn(module) ->
+    #      properties = module.properties(entity)
+    #      mana_cost = properties[:mana_cost]
+    #                  |> to_string
+    #                  |> String.ljust(6)
+    #
+    #      command = properties[:command]
+    #                |> to_string
+    #                |> String.ljust(8)
+    #
+    #      send_message(entity, "scroll", "<p><span class='dark-cyan'>#{mana_cost} #{command} #{module.name}</span></p>")
+    #    end)
   end
 
 end
