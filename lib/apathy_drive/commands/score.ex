5c904f8becab3f8ab38d6259393ff766843e52f3
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-02-16 17:59:44 -0600

    update 'score' for monsters

--- lib/apathy_drive/commands/score.ex
+++ lib/apathy_drive/commands/score.ex
@@ -15,56 +15,46 @@ defmodule Commands.Score do
     |> Spirit.send_scroll("<p><span class='dark-green'>Level:</span> <span class='dark-cyan'>#{spirit.level}</span></p>")
   end
 
-  def execute(spirit, monster, _arguments) do
-    if Entity.has_component?(spirit, Components.Name) do
-      send_message(spirit, "scroll", "<p><span class='dark-cyan'>#{Components.Name.value(spirit)}</span></p>")
-    else
-      send_message(spirit, "scroll", "<p><span class='dark-cyan'>Spirit</span></p>")
-    end
+  def execute(%Monster{} = monster, _arguments) do
 
-    send_message(spirit, "scroll", "<p><span class='dark-green'>Level:</span> <span class='dark-cyan'>#{Components.Level.value(spirit) |> to_string |> String.ljust(11)}</span> <span class='dark-green'>Experience:</span> <span class='dark-cyan'>#{Components.Experience.value(spirit)}</span>\n\n</p>")
+    Monster.send_scroll(monster, "<p><span class='dark-cyan'>#{monster.name |> String.ljust(18)}</span> <span class='dark-green'>Experience:</span> <span class='dark-cyan'>#{monster.experience}</span></p>")
 
-    send_message(spirit, "scroll", "<p><span class='dark-cyan'>#{Components.Name.value(monster) |> String.ljust(18)}</span> <span class='dark-green'>Experience:</span> <span class='dark-cyan'>#{Components.Experience.value(monster)}</span></p>")
-
-    monster_level = Components.Level.value(monster) |> to_string |> String.ljust(12)
+    monster_level = monster.level |> to_string |> String.ljust(12)
     monster_power = Systems.Trainer.monster_power(monster)
 
-    send_message(spirit, "scroll", "<p><span class='dark-green'>Level:</span> <span class='dark-cyan'>#{monster_level}</span><span class='dark-green'>Devs:</span> <span class='dark-cyan'>#{monster_power}</span></p>")
+    Monster.send_scroll(monster, "<p><span class='dark-green'>Level:</span> <span class='dark-cyan'>#{monster_level}</span><span class='dark-green'>Devs:</span> <span class='dark-cyan'>#{monster_power}</span></p>")
 
-    hp = String.ljust("#{Components.HP.value(monster)}/#{Systems.HP.max(monster)}", 15)
-    mana = "#{Components.Mana.value(monster)}/#{Systems.Mana.max(monster)}"
-    send_message(spirit, "scroll", "<p><span class='dark-green'>HP:</span> <span class='dark-cyan'>#{hp}</span><span class='dark-green'>Mana:</span>  <span class='dark-cyan'>#{mana}</span></p>")
+    hp = String.ljust("#{monster.hp}/#{Monster.max_hp(monster)}", 15)
+    mana = "#{monster.mana}/#{Monster.max_mana(monster)}"
+    Monster.send_scroll(monster, "<p><span class='dark-green'>HP:</span> <span class='dark-cyan'>#{hp}</span><span class='dark-green'>Mana:</span>  <span class='dark-cyan'>#{mana}</span></p>\n")
 
-    send_message(spirit, "scroll", "\n")
-
-    stat_names = Components.Stats.value(monster) |> Map.keys
+    stat_names = ["strength", "agility", "intelligence", "health"]
     chunks = get_chunks(stat_names)
     Enum.each chunks, &display_stats(monster, &1)
     display_effects(monster)
   end
 
-  def display_effects(monster) do
-    monster
-    |> Components.Effects.value
+  def display_effects(%Monster{} = monster) do
+    monster.effects
     |> Map.values
     |> Enum.filter(&(Map.has_key?(&1, :effect_message)))
     |> Enum.map(&(&1[:effect_message]))
     |> Enum.each(fn(message) ->
-         send_message(monster, "scroll", "<p>#{message}</p>")
+         Monster.send_scroll(monster, "<p>#{message}</p>")
        end)
   end
 
-  defp display_stats(entity, [stat1, stat2]) do
-    send_message(entity, "scroll", "<p>#{stattext(entity, stat1)} #{stattext(entity, stat2)}</p>")
+  defp display_stats(%Monster{} = monster, [stat1, stat2]) do
+    Monster.send_scroll(monster, "<p>#{stattext(monster, stat1)} #{stattext(monster, stat2)}</p>")
   end
 
-  defp display_stats(entity, [stat]) do
-    send_message(entity, "scroll", "<p>#{stattext(entity, stat)}</p>")
+  defp display_stats(%Monster{} = monster, [stat]) do
+    Monster.send_scroll(monster, "<p>#{stattext(monster, stat)}</p>")
   end
 
-  defp stattext(entity, stat) do
-    stat_rating = Systems.Stat.modified(entity, stat)
-    String.ljust("<span class='dark-green'>#{String.ljust("#{String.capitalize(stat)}:", 10)}</span> <span class='dark-cyan'>#{String.ljust("#{stat_rating}", 7)}</span>", 18)
+  defp stattext(%Monster{} = monster, stat) do
+    stat_rating = Monster.modified_stat(monster, stat)
+    String.ljust("<span class='dark-green'>#{String.ljust("#{String.capitalize(stat)}:", 15)}</span> <span class='dark-cyan'>#{String.ljust("#{stat_rating}", 7)}</span>", 18)
   end
 
   defp get_chunks([]), do: []
