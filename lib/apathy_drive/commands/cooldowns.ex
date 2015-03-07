4e8ba5ef2f187865b74a7c70ddc571946e6f5b9a
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-03-07 11:36:22 -0600

    remove attack command, update cooldown command

--- lib/apathy_drive/commands/cooldowns.ex
+++ lib/apathy_drive/commands/cooldowns.ex
@@ -8,21 +8,23 @@ defmodule Commands.Cooldowns do
     |> Spirit.send_scroll("<p>You need a body to do that.</p>")
   end
 
-  def execute(spirit, monster, _arguments) do
-    send_message(spirit, "scroll", "<p><span class='white'>The following abilities are on cooldown:</span></p>")
-    send_message(spirit, "scroll", "<p><span class='dark-magenta'>Ability Name    Remaining</span></p>")
-    display_cooldowns(monster)
+  def execute(%Monster{} = monster, _arguments) do
+    monster
+    |> Monster.send_scroll("<p><span class='white'>The following abilities are on cooldown:</span></p>")
+    |> Monster.send_scroll("<p><span class='dark-magenta'>Ability Name    Remaining</span></p>")
+    |> display_cooldowns
+
+    monster
   end
 
-  def display_cooldowns(entity) do
-    entity
-    |> Components.Effects.value
+  def display_cooldowns(%Monster{effects: effects} = monster) do
+    effects
     |> Map.values
     |> Enum.filter(fn(effect) ->
-         Map.has_key?(effect, :cooldown)
+         Map.has_key?(effect, "cooldown")
        end)
     |> Enum.each(fn(effect) ->
-         send_message(entity, "scroll", "<p><span class='dark-cyan'>#{effect[:cooldown] |> String.ljust(15)} #{effect[:cooldown_remaining]} seconds</span></p>")
+         Monster.send_scroll(monster, "<p><span class='dark-cyan'>#{effect["cooldown"] |> String.ljust(15)} #{effect["cooldown_remaining"]} seconds</span></p>")
        end)
   end
 
