23b58434151e91123c4ac02f8590f745e36ef302
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-04-03 18:18:51 -0500

    fix looking through open doors

--- lib/apathy_drive/exits/door.ex
+++ lib/apathy_drive/exits/door.ex
@@ -3,7 +3,7 @@ defmodule ApathyDrive.Exits.Door do
 
   def look(%Room{} = current_room, %Spirit{} = spirit, room_exit) do
     if open?(current_room, room_exit) do
-      super(spirit, current_room, room_exit)
+      super(current_room, spirit, room_exit)
     else
       Phoenix.Channel.reply spirit.socket, "scroll", %{:html => "<p>The #{name} is closed in that direction!</p>"}
     end
@@ -11,7 +11,7 @@ defmodule ApathyDrive.Exits.Door do
 
   def look(%Room{} = current_room, %Monster{} = monster, room_exit) do
     if open?(current_room, room_exit) do
-      super(monster, current_room, room_exit)
+      super(current_room, monster, room_exit)
     else
       ApathyDrive.Endpoint.broadcast! "monsters:#{monster.id}", "scroll", %{:html => "<p>The #{name} is closed in that direction!</p>"}
     end
