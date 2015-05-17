7caffef96c9438c4de723cf000c0069a9201e40b
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-05-16 20:25:48 -0500

    ability lists for factions

---
+++ web/controllers/factions_controller.ex
@@ -0,0 +1,32 @@
+defmodule ApathyDrive.FactionController do
+  use ApathyDrive.Web, :controller
+  alias ApathyDrive.Repo
+  import Ecto.Query
+  import Systems.Text
+
+  plug :action
+
+  def angels(conn, _params) do
+    render conn, "angels.html", abilities: abilities("Angel")
+  end
+
+  def demons(conn, _params) do
+    render conn, "demons.html", abilities: abilities("Demon")
+  end
+
+  def elementals(conn, _params) do
+    render conn, "elementals.html", abilities: abilities("Elemental")
+  end
+
+  defp abilities(faction) do
+    (from a in Ability,
+      where: a.faction == ^faction,
+      select: a)
+    |> Repo.all
+    |> Enum.reduce(%{}, fn(ability, abilities) ->
+         abilities = Map.put_new(abilities, ability.level, [])
+         update_in(abilities, [ability.level], &([ability | &1]))
+       end)
+  end
+
+end
