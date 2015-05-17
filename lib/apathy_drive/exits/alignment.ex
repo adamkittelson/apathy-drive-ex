8c918d2a6b34af95c5678f9a0eb28e9c63655abc
Adam Kittelson <adam.kittelson@apathydrive.com>
2015-05-16 21:01:28 -0500

    fix alignment exits

--- lib/apathy_drive/exits/alignment.ex
+++ lib/apathy_drive/exits/alignment.ex
@@ -1,18 +1,18 @@
 defmodule ApathyDrive.Exits.Alignment do
   use ApathyDrive.Exit
 
-  def move(%Room{}, %Spirit{alignment: "good"} = spirit, %{"min" => _min}) do
+  def move(%Room{}, %Spirit{alignment: "evil"} = spirit, %{"min" => _min}) do
     Spirit.send_scroll(spirit, "<p>You are not evil enough to use this exit.</p>")
   end
 
-  def move(%Room{}, %Spirit{alignment: "evil"} = spirit, %{"max" => _max}) do
+  def move(%Room{}, %Spirit{alignment: "good"} = spirit, %{"max" => _max}) do
     Spirit.send_scroll(spirit, "<p>You are too evil to use this exit.</p>")
   end
 
   def move(%Room{} = room, %Spirit{} = spirit, room_exit),  do: super(room, spirit, room_exit)
 
   def move(%Room{} = room, %Monster{} = monster, %{"min" => _min} = room_exit) do
-    if Monster.monster_alignment(monster) == "good" do
+    if Monster.monster_alignment(monster) == "evil" do
       Monster.send_scroll(monster, "<p>You are not evil enough to use this exit.</p>")
     else
       super(room, monster, room_exit)
@@ -20,7 +20,7 @@ defmodule ApathyDrive.Exits.Alignment do
   end
 
   def move(%Room{} = room, %Monster{} = monster, %{"max" => _max} = room_exit) do
-    if Monster.monster_alignment(monster) == "evil" do
+    if Monster.monster_alignment(monster) == "good" do
       Monster.send_scroll(monster, "<p>You are too evil to use this exit.</p>")
     else
       super(room, monster, room_exit)
