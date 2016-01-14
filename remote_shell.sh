d1b59e293a626699af83abd25dbb3a9b9cc9b211
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-01-13 22:55:07 -0600

    use ssh tunnel

--- remote_shell.sh
+++ remote_shell.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-iex --cookie $APATHY_DRIVE_COOKIE --name $USER@localhost -e ":observer.start" --remsh $APATHY_DRIVE_NODE --hidden
+SSH_TUNNEL_COMMAND="ssh -f -N -L 4369:localhost:4369 -L 49000:localhost:49000 -L 49001:localhost:49001 -L 49002:localhost:49002 -L 49003:localhost:49003 -L 49004:localhost:49004 apotheos.is"
 
+eval $SSH_TUNNEL_COMMAND
+set -e
+function cleanup {
+  kill `pgrep -f "$SSH_TUNNEL_COMMAND"`
+}
+trap cleanup EXIT
+
+iex --cookie $APATHY_DRIVE_COOKIE --name $USER@localhost -e ":observer.start" --remsh $APATHY_DRIVE_NODE_NAME@127.0.0.1 --hidden
