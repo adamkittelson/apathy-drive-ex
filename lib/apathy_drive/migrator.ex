7853f4f25e6d3a06c6ccebdac11e26880e8bc7ad
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-02-14 23:48:37 -0600

    hack around migrations

--- lib/apathy_drive/migrator.ex
+++ lib/apathy_drive/migrator.ex
@@ -1,8 +1,11 @@
 defmodule ApathyDrive.Migrator do
 
   def start_link do
+    Ecto.Migrator.run(ApathyDrive.Repo, "#{:code.priv_dir(:apathy_drive)}/repo/migrations", :up, all: true)
+
     Task.start_link(fn ->
-      Ecto.Migrator.run(ApathyDrive.Repo, "#{:code.priv_dir(:apathy_drive)}/repo/migrations", :up, all: true)
+      # this is the dirtiest hack of all time
+      :noop
     end)
   end
 
