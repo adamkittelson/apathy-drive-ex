d99fd63b3662b2d351b4d2a65beae88cebfa1084
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-03-10 12:11:52 -0800

    convert experience to bigint

---
+++ priv/repo/migrations/20160310195911_bigint-experience.exs
@@ -0,0 +1,12 @@
+defmodule :"Elixir.ApathyDrive.Repo.Migrations.Bigint-experience" do
+  use Ecto.Migration
+
+  def change do
+    alter table(:mobiles) do
+      modify :experience, :bigint
+    end
+    alter table(:spirits) do
+      modify :experience, :bigint
+    end
+  end
+end
