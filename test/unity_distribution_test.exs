a2df4ce9f34a4fde9e2e34a2bbf47e0f43841bf2
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-04-10 17:27:29 -0500

    fix test

--- test/unity_distribution_test.exs
+++ test/unity_distribution_test.exs
@@ -3,12 +3,12 @@ defmodule UnityDistributionTest do
 
   setup do
     contributions = %{
-      "zero"        => {:mobile, 0},
-      "five"        => {:mobile, 5},
-      "hundred"     => {:mobile, 100},
-      "thousand"    => {:room, 1000},
-      "5 thousand"  => {:room, 5000},
-      "7 thousand"  => {:room, 7000}
+      "zero"        => 0,
+      "five"        => 5,
+      "hundred"     => 100,
+      "thousand"    => 1000,
+      "5 thousand"  => 5000,
+      "7 thousand"  => 7000
     }
 
     {:ok, [contributions: contributions]}
@@ -17,16 +17,15 @@ defmodule UnityDistributionTest do
   test "correctly calculates essence distribution", context do
     distributions = ApathyDrive.Unity.calculate_distributions(context[:contributions])
 
-    assert distributions["zero"] == {:mobile, 0}
-    assert distributions["five"] == {:mobile, 5}
-    assert distributions["hundred"] == {:mobile, 100}
-    assert distributions["thousand"] == {:room, 1000}
-    assert distributions["5 thousand"] == {:room, 447}
-    assert distributions["7 thousand"] == {:room, -1552}
+    assert distributions["zero"] == 0
+    assert distributions["five"] == 5
+    assert distributions["hundred"] == 100
+    assert distributions["thousand"] == 1000
+    assert distributions["5 thousand"] == 447
+    assert distributions["7 thousand"] == -1552
     assert 0 ==
       distributions
       |> Map.values
-      |> Enum.map(&elem(&1, 1))
       |> Enum.sum
   end
 end
