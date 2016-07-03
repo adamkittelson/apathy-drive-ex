fbfde94aff39e9968cc9244580d35622a14a6360
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-07-03 14:54:38 -0500

    add items to admin

--- web/models/item.ex
+++ web/models/item.ex
@@ -14,8 +14,8 @@ defmodule ApathyDrive.Item do
     timestamps
   end
 
-  @required_fields ~w(name description weight worn_on level grade)
-  @optional_fields ~w()
+  @required_fields ~w(name description weight worn_on level grade global_drop)
+  @optional_fields ~w(abilities)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
