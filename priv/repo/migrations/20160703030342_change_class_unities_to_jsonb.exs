96b9136eb70034a9797c56d535fefb28fe6b30a3
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-07-03 13:59:41 -0500

    update migration to have up/down

--- priv/repo/migrations/20160703030342_change_class_unities_to_jsonb.exs
+++ priv/repo/migrations/20160703030342_change_class_unities_to_jsonb.exs
@@ -1,10 +1,17 @@
 defmodule ApathyDrive.Repo.Migrations.ChangeClassUnitiesToJsonb do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:classes) do
       remove :unities
       add :unities, :jsonb
+    end
+  end
+  
+  def down do
+    alter table(:classes) do
+      remove :unities
+      add :unities, {:array, :string}
     end
   end
 end
