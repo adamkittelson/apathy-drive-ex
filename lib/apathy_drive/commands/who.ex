93701b7ae418b8c701585f2c064abc62604c193d
Adam Kittelson <adam.kittelson@apathydrive.com>
2016-11-03 21:56:48 -0500

    update 'who'

--- lib/apathy_drive/commands/who.ex
+++ lib/apathy_drive/commands/who.ex
@@ -1,19 +1,30 @@
 defmodule ApathyDrive.Commands.Who do
   use ApathyDrive.Command
-  alias ApathyDrive.{Monster, Presence}
+  alias ApathyDrive.{Character, Mobile, Presence}
 
   def keywords, do: ["who"]
 
-  def execute(%Room{} = room, %Monster{} = monster, _arguments) do
-    Monster.send_scroll(monster, "<p><span class='dark-cyan'>Name</span></p>")
-    Monster.send_scroll(monster, "<p><span class='dark-green'>==============================================================</span></p>")
+  def execute(%Room{} = room, %Character{} = character, _arguments) do
+    Mobile.send_scroll(character, "<p><span class='yellow'>        Current Adventurers</span></p>")
+    Mobile.send_scroll(character, "<p><span class='dark-grey'>        ===================</span>\n\n</p>")
 
-    "spirits:online"
-    |> Presence.metas
+    list = Presence.metas("spirits:online")
+
+    longest_name_length =
+      list
+      |> Enum.max_by(&String.length(&1.name))
+      |> Map.get(:name)
+      |> String.length
+
+    list
+    |> Enum.sort_by(&(&1.name))
     |> Enum.each(fn
-         %{name: name} ->
-           Monster.send_scroll(monster, "<p>#{name}</p>")
-       end)
+        %{name: name} ->
+          alignment = "Good" |> String.rjust(7)
+          name = name |> String.ljust(longest_name_length)
+          title = "Adventurer"
+          Mobile.send_scroll(character, "<p>#{alignment} <span class='dark-green'>#{name} - <span class='dark-magenta'>#{title}</span></p>")
+        end)
 
     room
   end
