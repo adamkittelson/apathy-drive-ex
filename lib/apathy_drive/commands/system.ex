c349b595b72ced2730ded2d47ef28fa3f5c87991
Adam Kittelson <adam.kittelson@apathydrive.com>
2017-08-06 22:09:23 -0500

    refactor system commands a bit

--- lib/apathy_drive/commands/system.ex
+++ lib/apathy_drive/commands/system.ex
@@ -1,7 +1,8 @@
 defmodule ApathyDrive.Commands.System do
   use ApathyDrive.Command
   require Ecto.Query
-  alias ApathyDrive.{Area, AreaAlly, AreaEnemy, Character, Mobile, PubSub, Repo, Room}
+  alias ApathyDrive.{Area, Room}
+  alias ApathyDrive.Commands.System
 
   def keywords, do: ["system", "sys"]
 
@@ -14,247 +15,52 @@ defmodule ApathyDrive.Commands.System do
     room
   end
 
-  def system(%Room{id: id} = room, %Character{admin: true} = character, ["edit", "room"]) do
-    send(character.socket, {:open_tab, "/admin/rooms/#{id}"})
-    room
+  def system(%Room{} = room, character, ["area", "add", "ally" | ally_name]) do
+    System.Area.add_ally(room, character, ally_name)
   end
 
-  def system(%Room{area: %Area{level: old_level} = area} = room, character, ["area", "set", "level", level]) do
-    area = Area.update_level(area, level)
-    PubSub.broadcast!("areas:#{area.id}", {:update_area, area})
-    Mobile.send_scroll(character, "<p>#{area.name} updated from level #{old_level} to #{level}.</p>")
-    room
+  def system(%Room{} = room, character, ["area", "add", "enemy" | enemy_name]) do
+    System.Area.add_enemy(room, character, enemy_name)
   end
 
-  def system(%Room{area: %Area{} = area} = room, character, ["area", "add", "ally" | ally_name]) do
-    ally =
-      ally_name
-      |> Enum.join(" ")
-      |> Area.match_by_name
-
-    cond do
-      is_nil(ally) ->
-        Mobile.send_scroll(character, "<p>Could not find an area called #{Enum.join(ally_name, " ")}</p>")
-      ally.id == area.id ->
-        Mobile.send_scroll(character, "<p>An area cannot be its own ally.</p>")
-      :else ->
-        remove_enemy(area, ally)
-
-        Repo.insert_all(AreaAlly, [[area_id: area.id, ally_id: ally.id], [area_id: ally.id, ally_id: area.id]], on_conflict: :nothing)
-        |> case do
-          {2, nil} ->
-            Mobile.send_scroll(character, "<p>#{area.name} is now an ally of #{ally.name}.</p>")
-            PubSub.broadcast!("areas:#{area.id}", :reload_reputations)
-            PubSub.broadcast!("areas:#{ally.id}", :reload_reputations)
-          {0, nil} ->
-            Mobile.send_scroll(character, "<p>#{area.name} is already an ally of #{ally.name}.</p>")
-          error ->
-            Mobile.send_scroll(character, "<p>#{inspect(error)}</p>")
-        end
-    end
-    room
+  def system(%Room{} = room, character, ["area", "create" | area]) do
+    System.Area.create(room, character, area)
   end
 
-  def system(%Room{area: %Area{} = area} = room, character, ["area", "add", "enemy" | enemy_name]) do
-    enemy =
-      enemy_name
-      |> Enum.join(" ")
-      |> Area.match_by_name
-
-    cond do
-      is_nil(enemy) ->
-        Mobile.send_scroll(character, "<p>Could not find an area called #{Enum.join(enemy_name, " ")}</p>")
-      enemy.id == area.id ->
-        Mobile.send_scroll(character, "<p>An area cannot be its own enemy.</p>")
-      :else ->
-        remove_ally(area, enemy)
-
-        Repo.insert_all(AreaEnemy, [[area_id: area.id, enemy_id: enemy.id], [area_id: enemy.id, enemy_id: area.id]], on_conflict: :nothing)
-        |> case do
-          {2, nil} ->
-            Mobile.send_scroll(character, "<p>#{area.name} is now an enemy of #{enemy.name}.</p>")
-            PubSub.broadcast!("areas:#{area.id}", :reload_reputations)
-            PubSub.broadcast!("areas:#{enemy.id}", :reload_reputations)
-          {0, nil} ->
-            Mobile.send_scroll(character, "<p>#{area.name} is already an enemy of #{enemy.name}.</p>")
-          error ->
-            Mobile.send_scroll(character, "<p>#{inspect(error)}</p>")
-        end
-    end
-    room
+  def system(%Room{} = room, character, ["area", "list"]) do
+    System.Area.list(room, character)
   end
 
-  def system(%Room{area: %Area{} = area} = room, character, ["area", "remove", "enemy" | enemy_name]) do
-    enemy =
-      enemy_name
-      |> Enum.join(" ")
-      |> Area.match_by_name
-
-    cond do
-      is_nil(enemy) ->
-        Mobile.send_scroll(character, "<p>Could not find an area called #{Enum.join(enemy_name, " ")}</p>")
-      enemy.id == area.id ->
-        Mobile.send_scroll(character, "<p>An area cannot be its own enemy.</p>")
-      :else ->
-        remove_enemy(area, enemy)
-        PubSub.broadcast!("areas:#{area.id}", :reload_reputations)
-        PubSub.broadcast!("areas:#{enemy.id}", :reload_reputations)
-
-        Mobile.send_scroll(character, "<p>#{area.name} is no longer an enemy of #{enemy.name}.</p>")
-    end
-    room
+  def system(%Room{} = room, character, ["area", "merge" | area]) do
+    System.Area.merge(room, character, area)
   end
 
-  def system(%Room{area: %Area{} = area} = room, character, ["area", "remove", "ally" | ally_name]) do
-    ally =
-      ally_name
-      |> Enum.join(" ")
-      |> Area.match_by_name
-
-    cond do
-      is_nil(ally) ->
-        Mobile.send_scroll(character, "<p>Could not find an area called #{Enum.join(ally_name, " ")}</p>")
-      ally.id == area.id ->
-        Mobile.send_scroll(character, "<p>An area cannot be its own ally.</p>")
-      :else ->
-        remove_ally(area, ally)
-        PubSub.broadcast!("areas:#{area.id}", :reload_reputations)
-        PubSub.broadcast!("areas:#{ally.id}", :reload_reputations)
-
-        Mobile.send_scroll(character, "<p>#{area.name} is no longer an ally of #{ally.name}.</p>")
-    end
-    room
+  def system(%Room{} = room, character, ["area", "set", "level", level]) do
+    System.Area.set_level(room, character, level)
   end
 
-  def system(%Room{area: %Area{} = old_area} = room, character, ["area", "merge" | area]) do
-    area = Enum.join(area, " ")
-
-    area
-    |> Area.find_by_name
-    |> Repo.one
-    |> case do
-         %Area{} = area ->
-           PubSub.broadcast!("areas:#{old_area.id}", {:update_area, area})
-           Mobile.send_scroll(character, "<p>#{old_area.name} merged into #{area.name}.</p>")
-           room
-         nil ->
-           Mobile.send_scroll(character, "<p>Could not find an area named \"#{area}\".")
-           room
-       end
+  def system(%Room{} = room, character, ["area", "set" | area]) do
+    System.Area.set(room, character, area)
   end
 
-  def system(%Room{area: %Area{} = old_area} = room, character, ["set", "area" | area]) do
-    area = Enum.join(area, " ")
-
-    area
-    |> Area.find_by_name
-    |> Repo.one
-    |> case do
-         %Area{} = area ->
-           room = Room.update_area(room, area)
-
-           Mobile.send_scroll(character, "<p>Area changed from \"#{old_area.name}\"(#{old_area.level}) to \"#{room.area.name}\"(#{room.area.level}).</p>")
-           room
-         nil ->
-           Mobile.send_scroll(character, "<p>Could not find an area named \"#{area}\".")
-           room
-       end
+  def system(%Room{} = room, character, ["area", "remove", "ally" | ally_name]) do
+    System.Area.remove_ally(room, character, ally_name)
   end
 
-  def system(%Room{} = room, character, ["create", "area" | area]) do
-    area = Enum.join(area, " ")
-
-    area
-    |> Area.new_area_changeset
-    |> Repo.insert
-    |> case do
-         {:ok, %Area{name: name} = area} ->
-           room = Room.update_area(room, area)
-           Mobile.send_scroll(character, "<p>\"#{name}\" created!</p>")
-           room
-         {:error, %Ecto.Changeset{errors: errors}} ->
-           Enum.each(errors, fn {field, error} ->
-             message = ApathyDriveWeb.ErrorHelpers.translate_error(error)
-             Mobile.send_scroll(character, "<p>Error: #{field} #{message}</p>")
-           end)
-           room
-       end
+  def system(%Room{} = room, character, ["area", "remove", "enemy" | enemy_name]) do
+    System.Area.remove_enemy(room, character, enemy_name)
   end
 
-  def system(%Room{name: old_name} = room, character, ["set", "room", "name" | room_name]) do
-    room =
-      room
-      |> Map.put(:name, Enum.join(room_name, " "))
-      |> Repo.save!
-
-    ApathyDriveWeb.Endpoint.broadcast!("map", "room name change", %{room_id: room.id, name: room.name})
-
-    Mobile.send_scroll(character, "<p>Room name changed from \"#{old_name}\" to \"#{room.name}\".</p>")
-
-    room
+  def system(%Room{} = room, character, ["room", "set", "coords", x, y, z]) do
+    System.Room.set_coords(room, character, x, y, z)
   end
 
-  def system(%Room{coordinates: old_coords} = room, character, ["set", "room", "coords", x, y, z]) do
-    x = String.to_integer(x)
-    y = String.to_integer(y)
-    z = String.to_integer(z)
-
-    room =
-      room
-      |> Map.put(:coordinates, %{"x" => x, "y" => y, "z" => z})
-      |> Repo.save!
-
-    ApathyDriveWeb.Endpoint.broadcast!("map", "room coords change", %{room_id: room.id, x: x, y: y, z: z})
-
-    Mobile.send_scroll(character, "<p>Room coordinates changed from \"#{inspect(old_coords)}\" to \"#{inspect(room.coordinates)}\".</p>")
-
-    room
+  def system(%Room{} = room, character, ["room", "set", "coords" | []]) do
+    System.Room.set_coords(room, character)
   end
 
-  def system(%Room{coordinates: old_coords} = room, character, ["set", "room", "coords" | []]) do
-    room =
-      room
-      |> Map.put(:coordinates, nil)
-      |> Repo.save!
-
-    ApathyDriveWeb.Endpoint.broadcast!("map", "room coords change", %{room_id: room.id, x: 0, y: 0, z: 0})
-
-    Mobile.send_scroll(character, "<p>Room coordinates unset from \"#{inspect(old_coords)}\".</p>")
-
-    room
-  end
-
-  def system(%Room{} = room, character, ["list", "areas"]) do
-    Area.list_with_room_counts
-    |> Repo.all
-    |> Enum.chunk(10)
-    |> Enum.each(fn chunk ->
-         Mobile.send_scroll(character, "<p><span class='dark-magenta'>Level</span> <span class='dark-green'>|</span> <span class='dark-magenta'>Rooms</span> <span class='dark-green'>|</span> <span class='dark-magenta'>Area</span></p>")
-         Enum.each(chunk, fn [area, room_count] ->
-           area =
-             area
-             |> Repo.preload(:allies)
-             |> Repo.preload(:enemies)
-
-           allies =
-             area.allies
-             |> Enum.map(&(&1.name))
-
-           enemies =
-             area.enemies
-             |> Enum.map(&(&1.name))
-
-           Mobile.send_scroll(character, "<p><span class='dark-cyan'>#{to_string(area.level) |> String.pad_leading(5)}</span> <span class='dark-green'>|</span> <span class='dark-cyan'>#{to_string(room_count) |> String.pad_leading(5)}</span> <span class='dark-green'>|</span> <span class='black'>#{area.name}</span></p>")
-           if Enum.any?(allies) do
-             Mobile.send_scroll(character, "<p>              <span class='dark-green'>|</span>   <span class='dark-cyan'>Allies:</span> #{Enum.join(allies, ", ")}</p>")
-           end
-           if Enum.any?(enemies) do
-             Mobile.send_scroll(character, "<p>              <span class='dark-green'>|</span>   <span class='dark-cyan'>Enemies:</span> #{Enum.join(enemies, ", ")}</p>")
-           end
-         end)
-
-       end)
-    room
+  def system(%Room{} = room, character, ["room", "set", "name" | room_name]) do
+    System.Room.set_name(room, character, room_name)
   end
 
   def system(%Room{} = room, character, ["goto" | area]) do
@@ -295,22 +101,6 @@ defmodule ApathyDrive.Commands.System do
     Mobile.send_scroll(character, "<p>Invalid system command.</p>")
 
     room
-  end
-
-  defp remove_ally(%Area{} = area, %Area{} = ally) do
-    Ecto.Query.from(area_ally in AreaAlly, where: area_ally.area_id == ^area.id and area_ally.ally_id == ^ally.id)
-    |> Repo.delete_all
-
-    Ecto.Query.from(area_ally in AreaAlly, where: area_ally.area_id == ^ally.id and area_ally.ally_id == ^area.id)
-    |> Repo.delete_all
-  end
-
-  defp remove_enemy(%Area{} = area, %Area{} = enemy) do
-    Ecto.Query.from(area_enemy in AreaEnemy, where: area_enemy.area_id == ^area.id and area_enemy.enemy_id == ^enemy.id)
-    |> Repo.delete_all
-
-    Ecto.Query.from(area_enemy in AreaEnemy, where: area_enemy.area_id == ^enemy.id and area_enemy.enemy_id == ^area.id)
-    |> Repo.delete_all
   end
 
 end
---
+++ lib/apathy_drive/commands/system/room.ex
@@ -0,0 +1,47 @@
+defmodule ApathyDrive.Commands.System.Room do
+  alias ApathyDrive.{Mobile, Repo, Room}
+
+  def set_coords(%Room{coordinates: old_coords} = room, character, x, y, z) do
+    x = String.to_integer(x)
+    y = String.to_integer(y)
+    z = String.to_integer(z)
+
+    room =
+      room
+      |> Map.put(:coordinates, %{"x" => x, "y" => y, "z" => z})
+      |> Repo.save!
+
+    ApathyDriveWeb.Endpoint.broadcast!("map", "room coords change", %{room_id: room.id, x: x, y: y, z: z})
+
+    Mobile.send_scroll(character, "<p>Room coordinates changed from \"#{inspect(old_coords)}\" to \"#{inspect(room.coordinates)}\".</p>")
+
+    room
+  end
+
+  def set_coords(%Room{coordinates: old_coords} = room, character) do
+    room =
+      room
+      |> Map.put(:coordinates, nil)
+      |> Repo.save!
+
+    ApathyDriveWeb.Endpoint.broadcast!("map", "room coords change", %{room_id: room.id, x: 0, y: 0, z: 0})
+
+    Mobile.send_scroll(character, "<p>Room coordinates unset from \"#{inspect(old_coords)}\".</p>")
+
+    room
+  end
+
+  def set_name(%Room{name: old_name} = room, character, room_name) do
+    room =
+      room
+      |> Map.put(:name, Enum.join(room_name, " "))
+      |> Repo.save!
+
+    ApathyDriveWeb.Endpoint.broadcast!("map", "room name change", %{room_id: room.id, name: room.name})
+
+    Mobile.send_scroll(character, "<p>Room name changed from \"#{old_name}\" to \"#{room.name}\".</p>")
+
+    room
+  end
+
+end
