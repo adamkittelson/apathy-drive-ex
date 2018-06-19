82014374e4a8138b4a928d0d65bcdf1f1db218c1
Adam Kittelson <adam.kittelson@apathydrive.com>
2018-06-19 17:37:05 -0500

    add class

---
+++ priv/repo/migrations/20180619223555_create_classes.exs
@@ -0,0 +1,13 @@
+defmodule ApathyDrive.Repo.Migrations.CreateClasses do
+  use Ecto.Migration
+
+  def change do
+    create table(:classes) do
+      add :name, :text
+      add :description, :text
+
+      timestamps()
+    end
+
+  end
+end
