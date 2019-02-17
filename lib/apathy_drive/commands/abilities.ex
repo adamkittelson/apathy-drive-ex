eedeb2db4f170c1b64a2586c41bcea5f8c014d8c
Adam Kittelson <adam.kittelson@apathydrive.com>
2019-02-16 20:14:50 -0500

    clean up old scroll reading stuff

--- lib/apathy_drive/commands/abilities.ex
+++ lib/apathy_drive/commands/abilities.ex
@@ -1,22 +1,14 @@
 defmodule ApathyDrive.Commands.Abilities do
   use ApathyDrive.Command
-  alias ApathyDrive.{ClassAbility, Character, Mobile, Repo}
+  alias ApathyDrive.{Character, Item, Mobile}
   require Ecto.Query
 
   def keywords, do: ["abilities", "spells"]
 
-  def execute(%Room{} = room, %Character{id: id} = character, _arguments) do
-    number =
-      ApathyDrive.CharacterAbility
-      |> Ecto.Query.where([ca], ca.character_id == ^id)
-      |> Ecto.Query.select([ca], count(ca.id))
-      |> Repo.one()
-
-    max = Character.max_active_abilities(character)
-
+  def execute(%Room{} = room, %Character{} = character, _arguments) do
     Mobile.send_scroll(
       character,
-      "<p><span class='white'>You have the following abilities activated (#{number}/#{max}):</span></p>"
+      "<p><span class='white'>You know the following abilities:</span></p>"
     )
 
     Mobile.send_scroll(
@@ -25,20 +17,14 @@ defmodule ApathyDrive.Commands.Abilities do
     )
 
     display_abilities(character)
-
-    Mobile.send_scroll(
-      character,
-      "<br/><br/><p><span class='white'>You have the following abilities available:</span></p>"
-    )
-
-    display_class_abilities(character)
+    display_scroll_abilities(character)
     room
   end
 
   def display_abilities(%Character{} = character) do
     character.abilities
     |> Map.values()
-    |> Enum.each(fn %{name: name, command: command, mana: mana} = _ability ->
+    |> Enum.each(fn %{name: name, command: command, mana: mana} ->
       mana_cost = String.pad_trailing(to_string(mana), 6)
 
       command =
@@ -53,14 +39,47 @@ defmodule ApathyDrive.Commands.Abilities do
     end)
   end
 
-  def display_class_abilities(%Character{} = character) do
+  def display_scroll_abilities(%Character{} = character) do
     abilities =
-      character.class_id
-      |> ClassAbility.abilities_at_level(character.level)
-      |> Enum.sort_by(& &1.level)
-      |> Enum.map(& &1.ability.name)
-      |> Enum.join(", ")
+      character
+      |> scrolls()
+      |> Enum.map(& &1.traits["Learn"])
+      |> Enum.map(&%{name: &1.name, command: &1.command, mana: &1.mana})
+      |> Enum.uniq()
 
-    Mobile.send_scroll(character, "<p><span class='dark-cyan'>#{abilities}</span></p>")
+    if Enum.any?(abilities) do
+      Mobile.send_scroll(
+        character,
+        "<br/><br/><p><span class='white'>You have usable scrolls for the following abilities:</span></p>"
+      )
+
+      Mobile.send_scroll(
+        character,
+        "<p><span class='dark-magenta'>Mana   Command  Ability Name</span></p>"
+      )
+
+      abilities
+      |> Enum.each(fn %{name: name, command: command, mana: mana} = _ability ->
+        mana_cost = String.pad_trailing(to_string(mana), 6)
+
+        command =
+          command
+          |> to_string
+          |> String.pad_trailing(8)
+
+        Mobile.send_scroll(
+          character,
+          "<p><span class='dark-cyan'>#{mana_cost} #{command} #{name}</span></p>"
+        )
+      end)
+    end
+  end
+
+  def scrolls(%Character{inventory: inventory} = character) do
+    Enum.filter(
+      inventory,
+      &(&1.type == "Scroll" && !is_nil(&1.traits["Learn"]) &&
+          !Item.too_powerful_for_character?(character, &1))
+    )
   end
 end
--- lib/apathy_drive/schemas/character.ex
+++ lib/apathy_drive/schemas/character.ex
@@ -214,17 +214,15 @@ defmodule ApathyDrive.Character do
     |> validate_length(:name, min: 1, max: 12)
   end
 
-  def load_abilities(%Character{id: id} = character) do
+  def load_abilities(%Character{} = character) do
     character =
       character
       |> Map.put(:abilities, %{})
       |> Map.put(:skills, %{})
 
     abilities =
-      ApathyDrive.CharacterAbility
-      |> Ecto.Query.where([ca], ca.character_id == ^id)
-      |> Ecto.Query.preload([:ability])
-      |> Repo.all()
+      character.class_id
+      |> ApathyDrive.ClassAbility.abilities_at_level(character.level)
 
     character =
       abilities
