d2bfeee0f1c466445dc9d257e01a161e890b913b
Adam Kittelson <adam.kittelson@apathydrive.com>
2019-02-18 21:11:39 -0500

    nevermind :P

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,10 +46,6 @@ namespace :db do
 
   desc "Upload local data to server"
   task :local_to_server do
-    on roles(:app) do |host|
-      execute :pg_dump, "--no-privileges", "-U apathy_drive", "-w", "-h localhost", "-t characters", "-Ft apathy_drive > /home/deploy/characters.tar"
-      execute :pg_dump, "--no-privileges", "-U apathy_drive", "-w", "-h localhost", "-t items_instances", "-Ft apathy_drive > /home/deploy/items.tar"
-    end
     run_locally do
       execute :pg_dump, "--no-privileges", "-Ft apathy_drive > database.tar"
       execute :scp, "database.tar", "apotheos.is:/home/deploy/database.tar"
@@ -59,8 +55,6 @@ namespace :db do
       execute :psql, "-U apathy_drive", "-d template1", "-w", "-h localhost", "-c \"DROP DATABASE apathy_drive;\"" rescue nil
       execute :createdb, "-h localhost", "-U apathy_drive", "-w", "-O apathy_drive", "apathy_drive"
       execute :pg_restore, "-U apathy_drive", "-w", "-h localhost", "-O", "-d apathy_drive", "--role=apathy_drive", "-Ft /home/deploy/database.tar"
-      execute :pg_restore, "-U apathy_drive", "-w", "-h localhost", "-O", "-d apathy_drive", "--role=apathy_drive", "-Ft /home/deploy/characters.tar"
-      execute :pg_restore, "-U apathy_drive", "-w", "-h localhost", "-O", "-d apathy_drive", "--role=apathy_drive", "-Ft /home/deploy/items.tar"
     end
     invoke "deploy:start"
   end
