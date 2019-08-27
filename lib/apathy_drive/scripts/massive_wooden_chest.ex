cfaa82469c8a1b524822dd9ed42c126f6cb1be01
Adam Kittelson <adam.kittelson@apathydrive.com>
2019-08-27 11:56:49 -0400

    massive wooden chest

---
+++ lib/apathy_drive/scripts/massive_wooden_chest.ex
@@ -0,0 +1,22 @@
+defmodule ApathyDrive.Scripts.MassiveWoodenChest do
+  import ApathyDrive.Scripts
+  alias ApathyDrive.{Mobile, Room}
+
+  def execute(%Room{} = room, mobile_ref) do
+    Room.update_mobile(room, mobile_ref, fn mobile ->
+      Mobile.send_scroll(mobile, "<p>You open the massive wooden chest, and find...</p>")
+
+      room
+      |> give_coins_up_to(mobile_ref, %{gold: 120, silver: 1200})
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+    end)
+  end
+end
