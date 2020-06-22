9a5219653ae0b0daa2585f9e0203360bf9582dab
Adam Kittelson <adam.kittelson@apathydrive.com>
2020-06-21 21:41:17 -0400

    fix tests

--- test/lib/apathy_drive/exp_test.exs
+++ test/lib/apathy_drive/exp_test.exs
@@ -4,20 +4,20 @@ defmodule ApathyDrive.ExpTest do
   alias ApathyDrive.Character
 
   test "drain rate" do
-    assert 1 == Character.drain_rate(1)
-    assert 2 == Character.drain_rate(10)
-    assert 5 == Character.drain_rate(20)
-    assert 11 == Character.drain_rate(30)
-    assert 24 == Character.drain_rate(40)
-    assert 51 == Character.drain_rate(50)
+    assert 1 == trunc(Character.drain_rate(1))
+    assert 2 == trunc(Character.drain_rate(10))
+    assert 5 == trunc(Character.drain_rate(20))
+    assert 11 == trunc(Character.drain_rate(30))
+    assert 23 == trunc(Character.drain_rate(40))
+    assert 51 == trunc(Character.drain_rate(50))
   end
 
   test "max exp buffer" do
     assert 3600 == Character.max_exp_buffer(%Character{level: 1})
-    assert 7200 == Character.max_exp_buffer(%Character{level: 10})
-    assert 18000 == Character.max_exp_buffer(%Character{level: 20})
-    assert 39600 == Character.max_exp_buffer(%Character{level: 30})
-    assert 86400 == Character.max_exp_buffer(%Character{level: 40})
-    assert 183_600 == Character.max_exp_buffer(%Character{level: 50})
+    assert 7437 == Character.max_exp_buffer(%Character{level: 10})
+    assert 18260 == Character.max_exp_buffer(%Character{level: 20})
+    assert 40103 == Character.max_exp_buffer(%Character{level: 30})
+    assert 85977 == Character.max_exp_buffer(%Character{level: 40})
+    assert 183_757 == Character.max_exp_buffer(%Character{level: 50})
   end
 end
