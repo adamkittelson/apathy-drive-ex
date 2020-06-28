1f760162414857340ac76ebb9b7bb2d25b734710
Adam Kittelson <adam.kittelson@apathydrive.com>
2020-06-28 00:04:53 -0400

    mad attack tweak

--- lib/apathy_drive/scripts/mad_attack.ex
+++ lib/apathy_drive/scripts/mad_attack.ex
@@ -1,12 +1,12 @@
 defmodule ApathyDrive.Scripts.MadAttack do
   alias ApathyDrive.{Mobile, Room}
 
-  def execute(%Room{} = room, mobile_ref, target_ref) do
+  def execute(%Room{} = room, mobile_ref, _target_ref) do
     rounds = Enum.random(2..7)
 
     Enum.reduce(1..rounds, room, fn _round, room ->
       mobile = room.mobiles[mobile_ref]
-      target = room.mobiles[target_ref]
+      target = room.mobiles[Mobile.auto_attack_target(mobile, room)]
 
       if mobile && target do
         Mobile.send_scroll(
@@ -28,7 +28,7 @@ defmodule ApathyDrive.Scripts.MadAttack do
         mobile = Map.put(mobile, :energy, mobile.max_energy)
         room = put_in(room.mobiles[mobile_ref].energy, mobile.max_energy)
 
-        ApathyDrive.AI.auto_attack(mobile, room, target_ref) || room
+        ApathyDrive.AI.auto_attack(mobile, room) || room
       else
         room
       end
