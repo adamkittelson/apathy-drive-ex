3414a028892b458b437d143dea857048da75e2f7
Adam Kittelson <adam.kittelson@apathydrive.com>
2020-07-07 08:00:01 -0400

    double drain rate

--- lib/apathy_drive/schemas/character.ex
+++ lib/apathy_drive/schemas/character.ex
@@ -1494,7 +1494,7 @@ defmodule ApathyDrive.Character do
 
     exp_to_level = Level.exp_at_level(level) - Level.exp_at_level(level - 1)
 
-    target_time = 20 * level
+    target_time = 10 * level
 
     max(exp_to_level / (target_time * 60), 1.0)
   end
