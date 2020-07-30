56ab1143846aa3625b1f8c913dd441108be45beb
Adam Kittelson <adam.kittelson@apathydrive.com>
2020-07-29 20:23:27 -0400

    fix potential mad rage crash

--- lib/apathy_drive/scripts/mad_attack.ex
+++ lib/apathy_drive/scripts/mad_attack.ex
@@ -6,29 +6,34 @@ defmodule ApathyDrive.Scripts.MadAttack do
 
     Enum.reduce(1..rounds, room, fn _round, room ->
       mobile = room.mobiles[mobile_ref]
-      target = room.mobiles[Mobile.auto_attack_target(mobile, room)]
 
-      if mobile && target do
-        Mobile.send_scroll(
-          mobile,
-          "<p><span class='yellow'>You attack #{target.name} in a crazed, magic induced rage!</span></p>"
-        )
+      if mobile do
+        target = room.mobiles[Mobile.auto_attack_target(mobile, room)]
 
-        Mobile.send_scroll(
-          target,
-          "<p><span class='yellow'>#{mobile.name} attacks you in a crazed, magic induced rage!</span></p>"
-        )
+        if target do
+          Mobile.send_scroll(
+            mobile,
+            "<p><span class='yellow'>You attack #{target.name} in a crazed, magic induced rage!</span></p>"
+          )
 
-        Room.send_scroll(
-          room,
-          "<p><span class='yellow'>#{mobile.name} attacks #{target.name} in a crazed, magic induced rage!</span></p>",
-          [mobile, target]
-        )
+          Mobile.send_scroll(
+            target,
+            "<p><span class='yellow'>#{mobile.name} attacks you in a crazed, magic induced rage!</span></p>"
+          )
 
-        mobile = Map.put(mobile, :energy, mobile.max_energy)
-        room = put_in(room.mobiles[mobile_ref].energy, mobile.max_energy)
+          Room.send_scroll(
+            room,
+            "<p><span class='yellow'>#{mobile.name} attacks #{target.name} in a crazed, magic induced rage!</span></p>",
+            [mobile, target]
+          )
 
-        ApathyDrive.AI.auto_attack(mobile, room) || room
+          mobile = Map.put(mobile, :energy, mobile.max_energy)
+          room = put_in(room.mobiles[mobile_ref].energy, mobile.max_energy)
+
+          ApathyDrive.AI.auto_attack(mobile, room) || room
+        else
+          room
+        end
       else
         room
       end
