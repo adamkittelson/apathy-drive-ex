eca77af74aafd0c007f9263736c8a1c4dc933620
Adam Kittelson <adam.kittelson@apathydrive.com>
2020-10-02 21:59:41 -0400

    adamantite chest

---
+++ lib/apathy_drive/scripts/chests/adamantite_chest.ex
@@ -0,0 +1,41 @@
+defmodule ApathyDrive.Scripts.AdamantiteChest do
+  import ApathyDrive.Scripts
+  alias ApathyDrive.{Mobile, Room}
+
+  def execute(%Room{} = room, mobile_ref, _target_ref) do
+    Room.update_mobile(room, mobile_ref, fn room, mobile ->
+      Mobile.send_scroll(mobile, "<p>You open the adamantite chest, and find...</p>")
+
+      room
+      |> give_coins_up_to(mobile_ref, %{gold: 300, silver: 3000})
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_874(mobile_ref)
+      |> random_item_886(mobile_ref)
+      |> random_item_886(mobile_ref)
+      |> random_item_886(mobile_ref)
+      |> random_item_886(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_889(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_895(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+      |> random_item_898(mobile_ref)
+    end)
+  end
+end
