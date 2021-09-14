a2fec189a3ac53642130263c3c9db7d92460c2a8
Adam Kittelson <adam.kittelson@apathydrive.com>
2021-09-14 18:47:34 -0400

    utility function to help assign quality levels to items

--- lib/apathy_drive/system.ex
+++ lib/apathy_drive/system.ex
@@ -46,4 +46,45 @@ defmodule ApathyDrive.System do
     IO.puts("#{description}: #{div(time, 1000)}")
     result
   end
+
+  def items() do
+    require Ecto.Query
+
+    items =
+      ApathyDrive.ShopItem
+      |> Ecto.Query.preload(:item)
+      |> Repo.all()
+      |> Enum.map(& &1.item)
+      |> Enum.uniq()
+      |> Enum.filter(fn item ->
+        item.weapon_type == "blade" and item.game_limit == 0
+      end)
+      |> Enum.map(fn item ->
+        %{
+          id: item.id,
+          name: item.name,
+          min: item.min_damage,
+          max: item.max_damage,
+          speed: item.speed,
+          sort: [
+            ApathyDrive.Item.cost_in_copper(item),
+            (item.min_damage + item.max_damage) / item.speed
+          ]
+        }
+      end)
+      |> Enum.sort_by(& &1.sort)
+
+    count = length(items)
+
+    items
+    |> Enum.with_index(fn item, index ->
+      quality = trunc(index / count * 100) + 3
+
+      IO.puts(
+        "#{item.id},#{item.name},#{quality},#{item.speed},#{item.min},#{item.max},#{inspect(item.sort)}"
+      )
+    end)
+
+    nil
+  end
 end
