3601950e310adfa2793a8135c1fed6acc69c2a29
Adam Kittelson <adam.kittelson@apathydrive.com>
2021-09-16 13:37:00 -0400

    update utility function

--- lib/apathy_drive/system.ex
+++ lib/apathy_drive/system.ex
@@ -52,37 +52,50 @@ defmodule ApathyDrive.System do
 
     items =
       ApathyDrive.ShopItem
+      |> Ecto.Query.where([si], not is_nil(si.restock_frequency_in_minutes))
       |> Ecto.Query.preload(:item)
       |> Repo.all()
       |> Enum.map(& &1.item)
       |> Enum.uniq()
       |> Enum.filter(fn item ->
-        item.weapon_type == "blade" and item.game_limit == 0
+        item.worn_on == "Wrist" and item.game_limit == 0
       end)
+      |> Enum.reject(
+        &(&1.name in [
+            "wooden hammer",
+            "lockpicks",
+            "thief's kit",
+            "pig on a spit",
+            "gold jeweled ring",
+            "smoky black talisman"
+          ])
+      )
       |> Enum.map(fn item ->
+        defense = ApathyDrive.ItemTrait.load_traits(item.id)["Defense"]
+
         %{
           id: item.id,
           name: item.name,
-          min: item.min_damage,
-          max: item.max_damage,
-          speed: item.speed,
+          defense: defense,
+          cost: ApathyDrive.Item.cost_in_copper(item),
           sort: [
             ApathyDrive.Item.cost_in_copper(item),
-            (item.min_damage + item.max_damage) / item.speed
+            defense
           ]
         }
       end)
-      |> Enum.sort_by(& &1.sort)
+      |> Enum.group_by(& &1.sort)
+      |> Enum.sort()
 
     count = length(items)
 
     items
-    |> Enum.with_index(fn item, index ->
-      quality = trunc(index / count * 100) + 3
+    |> Enum.with_index(fn {_sort, items}, index ->
+      Enum.each(items, fn item ->
+        quality = trunc(index / count * 100) + 3
 
-      IO.puts(
-        "#{item.id},#{item.name},#{quality},#{item.speed},#{item.min},#{item.max},#{inspect(item.sort)}"
-      )
+        IO.puts("#{item.id},#{item.name},#{quality},#{item.defense},#{item.cost}")
+      end)
     end)
 
     nil
